4f9ac600a8f59a5d657558b6403bfe5df0181de8
xingang <xingang@elixiroftechnology.com>
2026-05-06 16:23:49 +0800

    添加 libbacktest.h 统一头文件；修复 CMakeLists.txt macOS 专属设置泄露到 Windows

---
+++ include/libbacktest.h
@@ -0,0 +1,13 @@
+//
+// Created by 薛新岗 on 2024/12/19.
+//
+
+#ifndef BACKTEST_LIBBACKTEST_H
+#define BACKTEST_LIBBACKTEST_H
+#include <boost/shared_ptr.hpp>
+#include "line/line.h"
+#include "lineManager/lineManager.h"
+#include "stocks.h"
+#include "strategy/TALibStrategy.h"
+#include "strategy/StrategyFactory.h"
+#endif //BACKTEST_LIBBACKTEST_H
